809a101f0cd90ef9c80f800bae3719207b926887
Manny <lihach77@gmail.com>
2019-04-20 12:18:36 +0300

    heroku

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
+const history = require('connect-history-api-fallback')
+
+const staticFileMiddleware = express.static(path.join(__dirname + '/dist'))
+
 app = express()
 app.use(serveStatic(__dirname))
 var port = process.env.PORT || 3000
 app.listen(port)
 console.log('server started ' + port)
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port
+  console.log('App now running on port', port)
+})
